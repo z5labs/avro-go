c5eea956ce55683527a04740d5588b1796b2d36d
Richard Carson Derr <cakub6@gmx.com>
2026-03-18 22:12:15 -0400

    feat(idl): rough out tokenizer

---
+++ idl/tokenizer.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2026 Z5Labs and Contributors
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package idl
+
+import (
+	"errors"
+	"io"
+	"iter"
+)
+
+// Pos represents the position of a token in the input.
+type Pos struct {
+	Line   int
+	Column int
+}
+
+// Token represents a token in the Avro IDL.
+type Token struct {
+	Pos   Pos
+	Type  TokenType
+	Value []byte
+}
+
+// TokenType represents the type of a token.
+type TokenType int
+
+// ErrEndOfTokens is the error returned by the tokenizer when it reaches the end of the input.
+var ErrEndOfTokens = errors.New("idl: end of tokens")
+
+// Tokenize the Avro IDL defined in the given reader.
+func Tokenize(r io.Reader) iter.Seq2[Token, error] {
+	return func(yield func(Token, error) bool) {}
+}
