f8fccc57bb2497267799a12a639441ee4b8c8eb1
Richard Carson Derr <cakub6@gmx.com>
2026-03-18 22:13:27 -0400

    story(issue-1): rough out tokenizer (#2)

---
+++ idl/doc.go
@@ -0,0 +1,7 @@
+// Copyright (c) 2026 Z5Labs and Contributors
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// Package idl provides tools for working with the Avro IDL.
+package idl
