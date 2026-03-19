51073231cb570a18b086c40b8327d0be6750c200
Richard Carson Derr <cakub6@gmx.com>
2026-03-18 22:22:50 -0400

    story(issue-3): rough out parser (#6)

---
+++ idl/parser.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2026 Z5Labs and Contributors
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package idl
+
+import "io"
+
+// File represents a parsed Avro IDL file.
+type File struct{}
+
+// Parse the Avro IDL defined in the given reader.
+func Parse(r io.Reader) (File, error) {
+	return File{}, nil
+}
