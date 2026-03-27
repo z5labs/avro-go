7ec2fb8deee91ff9fbb8df10019f27cb73f9120f
Richard Carson Derr <cakub6@gmx.com>
2026-03-26 22:49:59 -0400

    test(issue-13): added test cases for primitive schema tokenization

--- idl/tokenizer_test.go
+++ idl/tokenizer_test.go
@@ -6,6 +6,7 @@
 package idl
 
 import (
+	"iter"
 	"strings"
 	"testing"
 
@@ -13,6 +14,8 @@ import (
 )
 
 func TestTokenizer(t *testing.T) {
+	t.Parallel()
+
 	testCases := []struct {
 		name     string
 		src      string
@@ -21,33 +24,83 @@ func TestTokenizer(t *testing.T) {
 		{
 			name: "primitive schema with default namespace",
 			src:  `schema int;`,
+			expected: []Token{
+				{Pos: Pos{Line: 1, Column: 1}, Type: TokenIdentifier, Value: []byte("schema")},
+				{Pos: Pos{Line: 1, Column: 8}, Type: TokenIdentifier, Value: []byte("int")},
+				{Pos: Pos{Line: 1, Column: 11}, Type: TokenSymbol, Value: []byte(";")},
+			},
 		},
 		{
 			name: "primitive schema with custom namespace",
 			src: `namespace com.example;
 schema int;`,
+			expected: []Token{
+				{Pos: Pos{Line: 1, Column: 1}, Type: TokenIdentifier, Value: []byte("namespace")},
+				{Pos: Pos{Line: 1, Column: 11}, Type: TokenIdentifier, Value: []byte("com.example")},
+				{Pos: Pos{Line: 1, Column: 22}, Type: TokenSymbol, Value: []byte(";")},
+				{Pos: Pos{Line: 2, Column: 1}, Type: TokenIdentifier, Value: []byte("schema")},
+				{Pos: Pos{Line: 2, Column: 8}, Type: TokenIdentifier, Value: []byte("int")},
+				{Pos: Pos{Line: 2, Column: 11}, Type: TokenSymbol, Value: []byte(";")},
+			},
 		},
 		{
 			name: "primitive schema with single line comment",
 			src: `// This is a comment
 schema int;`,
+			expected: []Token{
+				{Pos: Pos{Line: 1, Column: 1}, Type: TokenComment, Value: []byte("// This is a comment")},
+				{Pos: Pos{Line: 2, Column: 1}, Type: TokenIdentifier, Value: []byte("schema")},
+				{Pos: Pos{Line: 2, Column: 8}, Type: TokenIdentifier, Value: []byte("int")},
+				{Pos: Pos{Line: 2, Column: 11}, Type: TokenSymbol, Value: []byte(";")},
+			},
 		},
 		{
-			name: "primitive schema with multi line comment",
+			name: "primitive schema with multi single line comment",
+			src: `/* This is a comment */
+schema int;`,
+			expected: []Token{
+				{Pos: Pos{Line: 1, Column: 1}, Type: TokenComment, Value: []byte("/* This is a comment */")},
+				{Pos: Pos{Line: 2, Column: 1}, Type: TokenIdentifier, Value: []byte("schema")},
+				{Pos: Pos{Line: 2, Column: 8}, Type: TokenIdentifier, Value: []byte("int")},
+				{Pos: Pos{Line: 2, Column: 11}, Type: TokenSymbol, Value: []byte(";")},
+			},
+		},
+		{
+			name: "primitive schema with multi multi line comment",
 			src: `/*
 * This is a comment
 */
 schema int;`,
+			expected: []Token{
+				{Pos: Pos{Line: 1, Column: 1}, Type: TokenComment, Value: []byte("/*\n* This is a comment\n*/")},
+				{Pos: Pos{Line: 4, Column: 1}, Type: TokenIdentifier, Value: []byte("schema")},
+				{Pos: Pos{Line: 4, Column: 8}, Type: TokenIdentifier, Value: []byte("int")},
+				{Pos: Pos{Line: 4, Column: 11}, Type: TokenSymbol, Value: []byte(";")},
+			},
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			tokens := make([]Token, 0, len(tc.expected))
-			for token, err := range Tokenize(strings.NewReader(tc.src)) {
-				require.Nil(t, err)
-				tokens = append(tokens, token)
+			t.Parallel()
+
+			collect := func(seq iter.Seq2[Token, error]) ([]Token, error) {
+				tokens := make([]Token, 0, len(tc.expected))
+				for item, err := range seq {
+					if err != nil {
+						return tokens, err
+					}
+					t.Log(item)
+					tokens = append(tokens, item)
+				}
+				return tokens, nil
 			}
+
+			tokens, err := collect(Tokenize(strings.NewReader(tc.src)))
+
+			var eot EndOfTokensError
+			require.ErrorAs(t, err, &eot)
+			require.NotEmpty(t, eot.Error())
 
 			require.Equal(t, tc.expected, tokens)
 		})
