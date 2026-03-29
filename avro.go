13b7cf6d30518553c383efd1d60f8d7fe0228579
Richard Carson Derr <cakub6@gmx.com>
2026-03-29 00:31:27 -0400

    feat(issue-51): implement all but int and long writing

---
+++ avro.go
@@ -0,0 +1,122 @@
+// Copyright (c) 2026 Z5Labs and Contributors
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package avro
+
+import (
+	"encoding/binary"
+	"io"
+	"math"
+)
+
+type BinaryMarshaler interface {
+	MarshalAvroBinary(w *BinaryWriter) error
+}
+
+// MarshalBinary writes the Avro data to the given writer.
+func MarshalBinary(w io.Writer, v BinaryMarshaler) error {
+	return v.MarshalAvroBinary(&BinaryWriter{out: w})
+}
+
+// BinaryWriter provides methods to write Avro data types to an underlying io.Writer.
+type BinaryWriter struct {
+	out io.Writer
+}
+
+func (w *BinaryWriter) WriteNull() error {
+	// TODO: the spec is confusing for this
+	return nil
+}
+
+// WriteBool writes a boolean value to the writer. It writes 1 for true and 0 for false.
+func (w *BinaryWriter) WriteBool(b bool) error {
+	var value byte
+	if b {
+		value = 1
+	}
+	n, err := w.out.Write([]byte{value})
+	if err != nil {
+		return err
+	}
+	if n != 1 {
+		return io.ErrShortWrite
+	}
+	return nil
+}
+
+// WriteInt writes a 32-bit integer to the writer using variable-length zigzag encoding.
+func (w *BinaryWriter) WriteInt(i int32) error {
+	// TODO: variable length zigzag encoding
+	return nil
+}
+
+// WriteLong writes a 64-bit integer to the writer using variable-length zigzag encoding.
+func (w *BinaryWriter) WriteLong(l int64) error {
+	// TODO: variable length zigzag encoding
+	return nil
+}
+
+// WriteFloat writes a 32-bit floating-point number to the writer in little-endian format.
+func (w *BinaryWriter) WriteFloat(f float32) error {
+	n := math.Float32bits(f)
+	buf := make([]byte, 4)
+	binary.LittleEndian.PutUint32(buf, n)
+	nw, err := w.out.Write(buf)
+	if err != nil {
+		return err
+	}
+	if nw != 4 {
+		return io.ErrShortWrite
+	}
+	return nil
+}
+
+// WriteDouble writes a 64-bit floating-point number to the writer in little-endian format.
+func (w *BinaryWriter) WriteDouble(d float64) error {
+	n := math.Float64bits(d)
+	buf := make([]byte, 8)
+	binary.LittleEndian.PutUint64(buf, n)
+	nw, err := w.out.Write(buf)
+	if err != nil {
+		return err
+	}
+	if nw != 8 {
+		return io.ErrShortWrite
+	}
+	return nil
+}
+
+// WriteBytes writes a byte array to the writer. It first writes the length of the array as a long, followed by the bytes.
+func (w *BinaryWriter) WriteBytes(b []byte) error {
+	err := w.WriteLong(int64(len(b)))
+	if err != nil {
+		return err
+	}
+	nw, err := w.out.Write(b)
+	if err != nil {
+		return err
+	}
+	if nw != len(b) {
+		return io.ErrShortWrite
+	}
+	return nil
+}
+
+// WriteString writes a string to the writer. It first writes the length of the string as a long, followed by the UTF-8 bytes of the string.
+func (w *BinaryWriter) WriteString(s string) error {
+	b := []byte(s)
+	err := w.WriteLong(int64(len(b)))
+	if err != nil {
+		return err
+	}
+	nw, err := w.out.Write(b)
+	if err != nil {
+		return err
+	}
+	if nw != len(b) {
+		return io.ErrShortWrite
+	}
+	return nil
+}
